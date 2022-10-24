8d30f1d4891b7c2bee863df960f971201d65dd59
BadTux <tekobase@outlook.com>
2022-10-24 21:49:45 +0800

    update ws_server.py to work with GNSS module.

--- ws_server.py
+++ ws_server.py
@@ -1,4 +1,6 @@
 import asyncio
+import json
+from pprint import pprint
 import serial_asyncio
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
@@ -52,7 +54,7 @@ async def get():
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    uart_rx, uart_tx = await serial_asyncio.open_serial_connection(url='/dev/ttyUSB1', baudrate=115200)
+    uart_rx, uart_tx = await serial_asyncio.open_serial_connection(url='/dev/ttyS1', baudrate=115200)
 
     async def read_from_socket(websocket: WebSocket):
         while True:
@@ -66,6 +68,9 @@ async def websocket_endpoint(websocket: WebSocket):
 
     while True:
         rx_bytes = await uart_rx.readuntil(b'\n')
-        rx_str = f"{rx_bytes}"
-        await websocket.send_text(rx_str)
-        print(rx_bytes)
+        try:
+            data = {"gnss_rx": rx_bytes.decode(), "gnss_rx_type": "string"}
+        except UnicodeDecodeError:
+            data = {"gnss_rx": f"{rx_bytes}", "gnss_rx_type": "bytes"}
+        await websocket.send_text(json.dumps(data))
+        pprint(data)
