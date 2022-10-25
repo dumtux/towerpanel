15ed5d94d65c5684f203bfc93a325820e072ef23
BadTux <tekobase@outlook.com>
2022-10-25 15:11:48 +0800

    use single terminal for GPS.

--- towerpanel-frontend/src/routes/(inner)/sensors/gnss/+page.svelte
+++ towerpanel-frontend/src/routes/(inner)/sensors/gnss/+page.svelte
@@ -1,141 +1,99 @@
 <script lang="ts">
 	import { storeFramework } from '$panel/stores';
+	import { writable, type Writable } from "svelte/store";
 	import { Divider, TabGroup, Tab } from '@brainandbones/skeleton';
 	import Alert from '@brainandbones/skeleton/components/Alert/Alert.svelte';
 	import ListBox from '@brainandbones/skeleton/components/ListBox/ListBox.svelte';
 	import ListBoxItem from '@brainandbones/skeleton/components/ListBox/ListBoxItem.svelte';
-	import { writable, type Writable } from "svelte/store";
 	import SlideToggle from '@brainandbones/skeleton/components/SlideToggle/SlideToggle.svelte';
 	import CodeBlock from '$lib/utilities/CodeBlock/FixedHeightCodeBlock.svelte';
 
-	const TERMINAL_MAX_ROWS = 10;
+	const CONSOLE_ROWS = 16;
 	const GNSS_DEFAULT_BAUDRATE = 19200;
 
 	let alert_decoding_visible = false;
-	let downstream_data = " ";
-	const rx_buffer: List<string> = [];
-	let upstream_data = " ";
-	const tx_buffer: List<string> = [];
+	let console_text = "";
+	const console_buffer: List<string> = [];
+	for (let i = 0; i < CONSOLE_ROWS; i++) {
+		console_buffer.push("_");
+	}
 
 	const baudrate: Writable<number> = writable(GNSS_DEFAULT_BAUDRATE);
 	let gnss_command = "";
-	const gnss_config = {
-		baudrate: GNSS_DEFAULT_BAUDRATE,
-		ethernet_enabled: false
-	};
 
 	const ws = new WebSocket("ws://192.168.0.6:8000/ws");
 
 	function sendCommand() {
-		tx_buffer.push(gnss_command + '\n');
+		console_buffer.push('Vega28 ◄◄ ' + gnss_command);
 		const json_tx = {
 			"gnss_tx": gnss_command + '\r\n'
 		}
 		ws.send(JSON.stringify(json_tx));
 		gnss_command = "";
-		if (tx_buffer.length > TERMINAL_MAX_ROWS) {
-			tx_buffer.shift();
+		if (console_buffer.length > CONSOLE_ROWS) {
+			console_buffer.shift();
 		}
-		upstream_data = tx_buffer.join('');
+		console_text = console_buffer.join('\n');
 	}
 
 	ws.addEventListener("message", function (event) {
 		const json_rx = JSON.parse(event.data);
 		if (json_rx["gnss_rx_type"] == "string") {
-			rx_buffer.push(json_rx["gnss_rx"].replace("\r\n", "\n"));
+			console_buffer.push('Vega28 ►► ' + json_rx["gnss_rx"].replace("\r\n", ""));
 			alert_decoding_visible = false;
 		} else if (json_rx["gnss_rx_type"] == "bytes") {
-			rx_buffer.push(json_rx["gnss_rx"].slice(2, json_rx["gnss_rx"].length-1) + '\n');
+			console_buffer.push('Vega28 ►► ' + json_rx["gnss_rx"].slice(2, json_rx["gnss_rx"].length-1));
 			alert_decoding_visible = true;
 		} else {
-			rx_buffer.push(json_rx["gnss_rx"]);
+			console_buffer.push('Vega28 ►► ' + json_rx["gnss_rx"]);
 			alert_decoding_visible = true;
 		}
-		if (rx_buffer.length > TERMINAL_MAX_ROWS) {
-			rx_buffer.shift();
+		if (console_buffer.length > CONSOLE_ROWS) {
+			console_buffer.shift();
 		}
-		downstream_data = rx_buffer.join('');
+		console_text = console_buffer.join('\n');
     });
 </script>
 
 <div class="page-container">
 	<!-- Install -->
 	<section class="space-y-4">
-		<h1>GNSS</h1>
+		<!-- <h1>GNSS</h1> -->
 		<!-- Tabs -->
 		<TabGroup selected={storeFramework}>
 			<Tab value="uart">Console</Tab>
-			<Tab value="config">Config</Tab>
 			<Tab value="webui">WebUI</Tab>
 		</TabGroup>
 		{#if $storeFramework === 'uart'}
-			<!-- <label for="baudrate">
-				<span>Baudrate</span>
-				<select name="baudrate" id="baudrate" bind:value={baudrate}>
-					<option value="4800">4800 bps</option>
-					<option value="9600">9600 bps</option>
-					<option value="14400">14400 bps</option>
-					<option value="19200">19200 bps</option>
-					<option value="38400">38400 bps</option>
-					<option value="57600">57600 bps</option>
-					<option value="115200">115200 bps</option>
-				</select>
-			</label> -->
 			<section class="grid grid-cols-1 lg:grid-cols-2 gap-4">
 				<div class="card card-body space-y-4">
-					<h2>TowerController ► Vega28</h2>
 					<CodeBlock
 						language="plaintext"
-						code="{upstream_data}"
+						code="{console_text}"
 					/>
 					<label for="gnss_command">
 						<input type="text" id="gnss_command" bind:value={gnss_command} on:change={sendCommand} minlength="2" required placeholder="Type command and hit Enter to send.">
 					</label>
 				</div>
-				<div class="card card-body space-y-4">
-					<h2>TowerController ◄ Vega28</h2>
-					<Alert background="bg-warning-500/30" border="border-l-4 border-warning-500" visible={alert_decoding_visible}>
-						<span>Failing to decode some bytes. Raw bytes data are shown. Check if UART baudrate correct.</span>
-					</Alert>
-					<CodeBlock
-						language="plaintext"
-						code={downstream_data}
-					/>
-				</div>
+				<section class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+					<div class="card card-body space-y-4">
+						<ListBox selected="{baudrate}" label="Baudrate">
+							<ListBoxItem value={4800}>4800 bps</ListBoxItem>
+							<ListBoxItem value={9600}>9600 bps</ListBoxItem>
+							<ListBoxItem value={14400}>14400 bps</ListBoxItem>
+							<ListBoxItem value={19200}>19200 bps</ListBoxItem>
+							<ListBoxItem value={38400}>38400 bps</ListBoxItem>
+							<ListBoxItem value={57600}>57600 bps</ListBoxItem>
+							<ListBoxItem value={115200}>115200 bps</ListBoxItem>
+						</ListBox>
+					</div>
+					<div class="card card-body space-y-4">
+						<p>(quick controls)</p>
+					</div>
+				</section>
 			</section>
 		{:else if $storeFramework === 'webui'}
 			<iframe class="tower-webui" src="http://192.168.0.25/index.cgi"></iframe>
-		{:else if $storeFramework === 'config'}
-			<section class="grid grid-cols-1 lg:grid-cols-3 gap-4">
-				<div class="card card-body space-y-4">
-					<h2>Status</h2>
-				</div>
-				<div class="card card-body space-y-4">
-					<h2>Ethernet</h2>
-
-					<div class="card card-body flex justify-center items-center space-x-4">
-						<SlideToggle size="md" bind:checked={gnss_config.ethernet_enabled}>Ethernet</SlideToggle>
-					</div>
-					<div class="card card-body flex justify-center items-center space-x-4">
-						<SlideToggle disabled label="Toggle Disabled">DHCP</SlideToggle>
-					</div>
-					<div class="card card-body flex justify-center items-center space-x-4">
-						<SlideToggle disabled label="Toggle Disabled">WebUI</SlideToggle>
-					</div>
-				</div>
-				<div class="card card-body space-y-4">
-					<h2>UART</h2>
-					<ListBox selected="{baudrate}">
-						<ListBoxItem value={4800}>4800 bps</ListBoxItem>
-						<ListBoxItem value={9600}>9600 bps</ListBoxItem>
-						<ListBoxItem value={14400}>14400 bps</ListBoxItem>
-						<ListBoxItem value={19200}>19200 bps</ListBoxItem>
-						<ListBoxItem value={38400}>38400 bps</ListBoxItem>
-						<ListBoxItem value={57600}>57600 bps</ListBoxItem>
-						<ListBoxItem value={115200}>115200 bps</ListBoxItem>
-					</ListBox>
-				</div>
-			</section>
 		{/if}
 	</section>
 
