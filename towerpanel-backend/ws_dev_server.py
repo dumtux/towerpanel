6ee2f3cec132c38c246fbfffbc45a7a21db69469
BadTux <tekobase@outlook.com>
2022-10-27 13:07:13 +0800

    make baudrange changing API endpoint.

--- towerpanel-backend/ws_dev_server.py
+++ towerpanel-backend/ws_dev_server.py
@@ -2,8 +2,9 @@ import asyncio
 import json
 from pprint import pprint
 import serial_asyncio
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, HTTPException
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 import uvicorn
 
 
@@ -15,11 +16,25 @@ DEVICES = {
     'test': {'dev': './ttyWriter', 'baud': 9600},
 }
 
+
+class BusConfig(BaseModel):
+    baudrate: int
+
+
+current_config = BusConfig(baudrate=9600)
+
 app = FastAPI()
 
 @app.get("/")
-async def get():
+async def home():
     return "WebSocket Dev server"
+
+
+@app.put("/bus/{device_name}")
+async def update_item(device_name: str, config: BusConfig):
+    if device_name not in DEVICES.keys():
+        raise HTTPException(status_code=404, detail=f"`{device_name}` does not exist.")
+    current_config.baudrate = config.baudrate
 
 
 @app.websocket("/ws/{device_name}")
@@ -38,11 +53,18 @@ async def websocket_gps(websocket: WebSocket, device_name: str):
             tx_str = await websocket.receive_text()
             tx_bytes = bytes(json.loads(tx_str))
             print(f"{device_name} << {tx_str}")
+            print(current_config.baudrate)
+            if current_config.baudrate != writer._transport.serial.baudrate:
+                writer._transport.serial.baudrate = current_config.baudrate
+            print(writer._transport.serial)
             writer.write(tx_bytes)
 
     asyncio.create_task(read_from_socket(websocket))
 
     while True:
+        if current_config.baudrate != writer._transport.serial.baudrate:
+            writer._transport.serial.baudrate = current_config.baudrate
+        print(current_config.baudrate)
         rx_bytes = await reader.readuntil(b'\n')
         rx_str = json.dumps([int(b) for b in rx_bytes])
         print(f"{device_name} >> {rx_str}")
