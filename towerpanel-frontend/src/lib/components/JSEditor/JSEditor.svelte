4c7150ebe2153deb584e958fc7081951960eeaca
BadTux <tekobase@outlook.com>
2022-10-27 23:32:22 +0800

    show alert on syntax error.

--- towerpanel-frontend/src/lib/components/JSEditor/JSEditor.svelte
+++ towerpanel-frontend/src/lib/components/JSEditor/JSEditor.svelte
@@ -7,6 +7,7 @@
     import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
     import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 	import { Divider, DataTable } from '@brainandbones/skeleton';
+	import Alert from '@brainandbones/skeleton/components/Alert/Alert.svelte';
 
     let divEl: HTMLDivElement = null;
     let editor: monaco.editor.IStandaloneCodeEditor;
@@ -59,6 +60,8 @@
     let headings: string[] = [];
     let source: any[] = [];
     let parser = (data) => [];
+    let alert_syntax_visible = false;
+    let syntax_error = '';
 
     function update(data) {
         let result = parser(data);
@@ -70,12 +73,23 @@
         function looseJsonParse(obj) {
             return Function(`"use strict";return (${obj})`)();
         }
-        parser = looseJsonParse(editor.getValue())
-        update("data")
+        try {
+            parser = looseJsonParse(editor.getValue())
+            alert_syntax_visible = false;
+            update("data")
+        }
+        catch (e) {
+            alert_syntax_visible = true;
+            syntax_error = e;
+        }
     }
 </script>
 
 <section class="space-y-4">
+    <Alert background="bg-warning-500/30" border="border-l-4 border-warning-500" visible={alert_syntax_visible}>
+        <h2>JS Parsing Error</h2>
+        <span>{syntax_error}</span>
+    </Alert>
     <section class="grid grid-cols-2 gap-4">
         <div class="col-span-1 card card-body space-y-4">
             <DataTable {headings} {source}></DataTable>
