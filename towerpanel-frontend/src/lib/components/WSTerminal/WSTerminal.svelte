185a3ef7ae6577f592c4eaa945b7f2b11414a4c9
BadTux <tekobase@outlook.com>
2022-10-27 20:15:45 +0800

    make JSEditor component.

--- towerpanel-frontend/src/lib/components/WSTerminal/WSTerminal.svelte
+++ towerpanel-frontend/src/lib/components/WSTerminal/WSTerminal.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import JSEditor from '$lib/components/JSEditor/JSEditor.svelte';
+
+	import { writable, type Writable } from "svelte/store";
 	import { Divider, TabGroup, Tab } from '@brainandbones/skeleton';
 	import Alert from '@brainandbones/skeleton/components/Alert/Alert.svelte';
 	import { onMount } from 'svelte';
+
+	const storeTab: Writable<string> = writable('console');
 
 	export let device_name: string = "test";
 	export let default_baudrate: string = "19200";
@@ -76,41 +81,55 @@
 </script>
 
 <div class="page-container">
-	<!-- Install -->
-	<section class="space-y-4">
-		<!-- <h1>GNSS</h1> -->
-		<Alert background="bg-warning-500/30" border="border-l-4 border-warning-500" visible={alert_connection_visible}>
-			<span>
-				Websocket connection is not established. If you see this message constantly, report on the
-				<a href="https://github.com/dumtux/towerpanel/issues" target="_blank" rel="noreferrer" class="">GitHub Issues</a>.
-			</span>
-		</Alert>
-		<Alert background="bg-warning-500/30" border="border-l-4 border-warning-500" visible={alert_decoding_visible}>
-			<span>Failing to decode some bytes. Raw bytes data are shown. Try different baudrate.</span>
-		</Alert>
-		<section class="grid grid-cols-5 gap-4">
-		<div class="col-span-4 card card-body space-y-4">
-			<pre><code style="min-height: 28em; white-space: pre; word-break: normal; word-wrap: normal;">{console_text}</code></pre>
-			<label for="gnss_command">
-				<input type="text" id="gnss_command" bind:value={gnss_command} on:change={sendCommand} minlength="2" required placeholder="Type command and hit Enter to send.">
-			</label>
-		</div>
-		<div class="card card-body space-y-4">
-			<label>Baudrate</label>
-			<select name="color" id="color" bind:value={baudrate} on:change={() => {setBaudrate(baudrate)}}>
-				{#each BUADRATE_LIST as value}
-					<option value="{value}">{value} bps</option>
-				{/each}
-			</select>
-			<Divider />
-			<label>Read Timeout</label>
-			<select name="color" id="color" bind:value={timeout} on:change={() => {setReadTimeout(timeout)}}>
-				{#each TIMEOUT_LIST as value}
-					<option value="{value}">{value} s</option>
-				{/each}
-			</select>
-		</div>
-	</section>
+	<TabGroup selected={storeTab}>
+		<Tab value="console">Console</Tab>
+		<Tab value="parser">Parser</Tab>
+	</TabGroup>
+
+	<!-- Conditionally display content -->
+	{#if $storeTab === 'console'}
+		<section class="space-y-4">
+			<!-- <h1>GNSS</h1> -->
+			<Alert background="bg-warning-500/30" border="border-l-4 border-warning-500" visible={alert_connection_visible}>
+				<span>
+					Websocket connection is not established. If you see this message constantly, report on the
+					<a href="https://github.com/dumtux/towerpanel/issues" target="_blank" rel="noreferrer" class="">GitHub Issues</a>.
+				</span>
+			</Alert>
+			<Alert background="bg-warning-500/30" border="border-l-4 border-warning-500" visible={alert_decoding_visible}>
+				<span>Failing to decode some bytes. Raw bytes data are shown. Try different baudrate.</span>
+			</Alert>
+			<section class="grid grid-cols-5 gap-4">
+				<div class="col-span-4 card card-body space-y-4">
+					<pre><code style="min-height: 28em; white-space: pre; word-break: normal; word-wrap: normal;">{console_text}</code></pre>
+					<label for="gnss_command">
+						<input type="text" id="gnss_command" bind:value={gnss_command} on:change={sendCommand} minlength="2" required placeholder="Type command and hit Enter to send.">
+					</label>
+				</div>
+				<div class="card card-body space-y-4">
+					<label>Baudrate</label>
+					<select name="color" id="color" bind:value={baudrate} on:change={() => {setBaudrate(baudrate)}}>
+						{#each BUADRATE_LIST as value}
+							<option value="{value}">{value} bps</option>
+						{/each}
+					</select>
+					<Divider />
+					<label>Read Timeout</label>
+					<select name="color" id="color" bind:value={timeout} on:change={() => {setReadTimeout(timeout)}}>
+						{#each TIMEOUT_LIST as value}
+							<option value="{value}">{value} s</option>
+						{/each}
+					</select>
+				</div>
+			</section>
+		</section>
+	{/if}
+
+	{#if $storeTab === 'parser'}
+		<section class="space-y-4">
+			<JSEditor />
+		</section>
+	{/if}
 
 	<Divider />
 
