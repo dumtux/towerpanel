00d4b5fe245a6268f955e36d09a11a30482b0a21
BadTux <tekobase@outlook.com>
2022-11-17 07:01:34 +0800

    frontend: add UHF setting form fields.

--- towerpanel-frontend/src/routes/(inner)/devices/uhf/+page.svelte
+++ towerpanel-frontend/src/routes/(inner)/devices/uhf/+page.svelte
@@ -2,6 +2,10 @@
 	import { Divider, RangeSlider } from '@brainandbones/skeleton';
 	import WSTerminal from '$lib/components/WSTerminal/WSTerminal.svelte';
 
+	let module: string = "TR4+";
+	let serial_number: string = "0123456789";
+	let rssi: number = -116.0;
+
 	const CHANEL_SPACING_LIST = [12.5, 20.0, 25];
 	let channel_spacing: number = CHANEL_SPACING_LIST[0];
 	const TX_POWER_LIST = [500.0, 1000.0];
@@ -25,6 +29,22 @@
 
 <div class="page-container">
 	<section class="card card-body space-y-4">
+		<div class="grid grid-cols-3 gap-4">
+			<div>
+				<label>Module</label>
+				<input type="text" bind:value={module} readonly />
+			</div>
+			<div>
+				<label>SN</label>
+				<input type="text" bind:value={serial_number} readonly />
+			</div>
+			<div>
+				<label>RSSI</label>
+				<input type="text" bind:value={rssi} readonly />
+			</div>
+		</div>
+	</section>
+	<section class="card card-body space-y-4">
 		<div class="justify-center items-center">
 			<div class="flex justify-between items-center">
 				<div>RX Frequency</div>
@@ -33,8 +53,6 @@
 			<RangeSlider bind:value={rx_frequency} min={MIN_FREQENcY} max={MAX_FREQENcY} step={0.025} ticked></RangeSlider>
 			<p class="text-center"><code>{formatFrequency(rx_frequency)}</code> MHz</p>
 		</div>
-		<Divider />
-
 		<div class="justify-center items-center">
 			<div class="flex justify-between items-center">
 				<div>TX Frequency</div>
@@ -43,30 +61,35 @@
 			<RangeSlider bind:value={tx_frequency} min={MIN_FREQENcY} max={MAX_FREQENcY} step={0.025} ticked></RangeSlider>
 			<p class="text-center"><code>{formatFrequency(tx_frequency)}</code> MHz</p>
 		</div>
+		<div class="grid grid-cols-3 gap-4">
+			<div>
+				<label>Channel Spacing</label>
+				<select name="channel_spacing" id="channel_spacing" bind:value={channel_spacing} on:change={onSettingsChange}>
+					{#each CHANEL_SPACING_LIST as value}
+						<option value="{value}">{value} kHz</option>
+					{/each}
+				</select>
+			</div>
+			<div>
+				<label>TX Power</label>
+				<select name="tx_power" id="tx_power" bind:value={tx_power} on:change={onSettingsChange}>
+					{#each TX_POWER_LIST as value}
+						<option value="{value}">{value} mW</option>
+					{/each}
+				</select>
+			</div>
+			<div>
+				<label>Protocol</label>
+				<select name="protocol" id="protocol" bind:value={protocol} on:change={onSettingsChange}>
+					{#each PROTOCOL_LIST as value}
+						<option value="{value}">{value}</option>
+					{/each}
+				</select>
+			</div>
+		</div>
 		<Divider />
-
-		<label>Channel Spacing</label>
-		<select name="channel_spacing" id="channel_spacing" bind:value={channel_spacing} on:change={onSettingsChange}>
-			{#each CHANEL_SPACING_LIST as value}
-				<option value="{value}">{value} kHz</option>
-			{/each}
-		</select>
-		<Divider />
-
-		<label>TX Power</label>
-		<select name="tx_power" id="tx_power" bind:value={tx_power} on:change={onSettingsChange}>
-			{#each TX_POWER_LIST as value}
-				<option value="{value}">{value} mW</option>
-			{/each}
-		</select>
-		<Divider />
-
-		<label>Protocol</label>
-		<select name="protocol" id="protocol" bind:value={protocol} on:change={onSettingsChange}>
-			{#each PROTOCOL_LIST as value}
-				<option value="{value}">{value}</option>
-			{/each}
-		</select>
+		<button class="btn bg-primary-500 btn-base text-white">Update</button>
+		<button class="btn bg-primary-500 btn-base text-white" disabled>Save Permanently</button>
 	</section>
 
 	<WSTerminal device_name="uhf" default_baudrate="19200" />
